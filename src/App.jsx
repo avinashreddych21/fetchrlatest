59bf2e58dca66202c96fcf67d36226d4da242083
avinashreddych21 <avinashreddych2006@gmail.com>
2026-03-29 17:31:12 +0530

    Updated App.jsx to import images at the top and use imported variables instead of string paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
-<img src="./assets/logo-icon.png"
+import Image1 from './path/to/image1.png';
+import Image2 from './path/to/image2.png';
+import Image3 from './path/to/image3.png';
+
+// Use the image variables instead of string paths
+function App() {
+  return (
+    <div>
+      <img src={Image1} alt='Image 1' />
+      <img src={Image2} alt='Image 2' />
+      <img src={Image3} alt='Image 3' />
+    </div>
+  );
+}
+
+export default App;
